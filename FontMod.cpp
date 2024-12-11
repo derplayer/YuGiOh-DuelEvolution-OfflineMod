99fcb09a6ee66edee2d03bd508f9ee2c350cc6cb
derplayer <derplayer@outlook.de>
2024-12-11 17:06:50 +0100

    release profile fix

--- FontMod.cpp
+++ FontMod.cpp
@@ -12,7 +12,16 @@
 namespace fs = std::filesystem;
 
 #include "Detours/src/detours.h"
-#pragma comment(lib, "include/libMinHook-x86-v141-mt.lib")
+#ifdef _DEBUG
+//#pragma comment(lib, "include/Debug/MinHook.x86.lib")
+//#pragma comment(lib, "include/libMinHook-x86-v141-mtd.lib")
+#pragma comment(lib, "lib/Debug/libMinHook.x86.lib")
+#else
+//#pragma comment(lib, "include/Release/MinHook.x86.lib")
+//#pragma comment(lib, "include/libMinHook-x86-v141-mt.lib")
+#pragma comment(lib, "lib/Release/libMinHook.x86.lib")
+#endif
+
 //#pragma comment(lib, "include/MinHook.x86.dll")
 #include "include/MinHook.h"
 #include "Util.hpp"
